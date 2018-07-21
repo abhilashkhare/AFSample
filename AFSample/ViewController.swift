569c216c8a033a52fca68eef3dec2f70ce6a31eb
Abhilash Khare <abhilashkhare@gmail.com>
2018-07-20 23:58:11 -0700

    feat: Import Alamofire Module and make an API request

--- AFSample/ViewController.swift
+++ AFSample/ViewController.swift
@@ -7,19 +7,16 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://codewithchris.com/code/afsample.json").responseJSON { (response) in
+            print(response.result.value)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
